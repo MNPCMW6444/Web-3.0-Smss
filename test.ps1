f922ffa0ed4ef36da703833ff06bcf2adc24e480
MNPCMW6444 <30693526+MNPCMW6444@users.noreply.github.com>
2022-03-22 18:53:47 +0200

    sd

--- test.ps1
+++ test.ps1
@@ -2,6 +2,7 @@ While ($true)
 
  { 
    python "C:\Users\Administrator\Desktop\test.py"
+   C:\Users\Administrator\Desktop\curl.exe "https://aqueous-fjord-71548.herokuapp.com/getBorrowableMims"
 
    start-sleep -seconds 300
 
